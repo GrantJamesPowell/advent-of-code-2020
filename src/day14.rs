a7bc8ba9a6d4042d5c1f346805a5bc93ef81300b
Grant Powell <email@grantjamespowell.com>
2021-12-16 14:06:30 -0500

    Day 14 (Naive Attempt #2)

--- src/day14.rs
+++ src/day14.rs
@@ -6,41 +6,57 @@ fn main() {
     let (starting, rules) = load_file("./src/inputs/day14.txt");
 
     // Part 1
-    let after = (0..10).fold(starting.clone(), |next, _i| expand(&next, &rules));
-    println!("Day 14 Pt 1 answer: {:?}", answer(&after));
-
-    // Part 2
-    let after = (0..40).fold(starting, |next, i| {
-        println!("Startin {:?}", i);
-        expand(&next, &rules)
-    });
-    println!("Day 14 Pt 2 answer: {:?}", answer(&after));
+    let counts = expand_template(&starting, &rules, 40);
+    let min = counts.iter().map(|(_k, v)| v).min().expect("not empty");
+    let max = counts.iter().map(|(_k, v)| v).max().expect("not empty");
+    println!("Day 14 Pt. 1 answer: {:?}", max - min);
 }
 
-fn answer(expanded: &[char]) -> usize {
+fn expand_template(
+    template: &[char],
+    rules: &HashMap<(char, char), char>,
+    times: usize,
+) -> HashMap<char, usize> {
     let mut counts = HashMap::new();
-    expanded
-        .iter()
-        .for_each(|c| *counts.entry(c).or_insert(0) += 1);
 
-    let counts: Vec<usize> = counts.into_iter().map(|(_k, v)| v).collect();
-    let most_common = counts.iter().max().expect("has at least one");
-    let least_common = counts.iter().min().expect("has at least one");
+    for c in template.iter() {
+        *counts.entry(*c).or_insert(0) += 1;
+    }
 
-    most_common - least_common
+    template.array_windows::<2>().for_each(|&[a, b]| {
+        println!("Expanding Initial Pair {:?}", (a, b));
+        let expanded_counts = expand_pair((a, b), rules, times);
+        println!(" -> Expanded Counts to: {:?}", expanded_counts);
+        add_counts(&mut counts, expanded_counts)
+    });
+
+    counts
 }
 
-fn expand(template: &[char], rules: &HashMap<(char, char), char>) -> Vec<char> {
-    let mut expanded = Vec::new();
-    template
-        .array_windows::<2>()
-        .for_each(|&[a, b]| match rules.get(&(a, b)) {
-            None => expanded.push(a),
-            Some(c) => expanded.extend_from_slice(&[a, *c]),
-        });
+fn expand_pair(
+    (a, b): (char, char),
+    rules: &HashMap<(char, char), char>,
+    times: usize,
+) -> HashMap<char, usize> {
+    if times == 0 {
+        return HashMap::new();
+    }
 
-    expanded.push(*template.last().expect("not empty"));
-    expanded
+    let mut counts = HashMap::new();
+
+    if let Some(c) = rules.get(&(a, b)) {
+        *counts.entry(*c).or_insert(0) += 1;
+        add_counts(&mut counts, expand_pair((a, *c), rules, times - 1));
+        add_counts(&mut counts, expand_pair((*c, b), rules, times - 1));
+    }
+
+    counts
+}
+
+fn add_counts(to: &mut HashMap<char, usize>, other: HashMap<char, usize>) {
+    for (k, v) in other {
+        *to.entry(k).or_insert(0) += v;
+    }
 }
 
 fn load_file(file_name: &str) -> (Vec<char>, HashMap<(char, char), char>) {
