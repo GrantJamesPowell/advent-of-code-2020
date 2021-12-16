67d2be06c532ab294424c7d6850e80a1529799fb
Grant Powell <email@grantjamespowell.com>
2021-12-16 14:21:09 -0500

    Day 14 (with memoization)

--- src/day14.rs
+++ src/day14.rs
@@ -6,10 +6,16 @@ fn main() {
     let (starting, rules) = load_file("./src/inputs/day14.txt");
 
     // Part 1
-    let counts = expand_template(&starting, &rules, 40);
+    let counts = expand_template(&starting, &rules, 10);
     let min = counts.iter().map(|(_k, v)| v).min().expect("not empty");
     let max = counts.iter().map(|(_k, v)| v).max().expect("not empty");
     println!("Day 14 Pt. 1 answer: {:?}", max - min);
+
+    // Part 1
+    let counts = expand_template(&starting, &rules, 40);
+    let min = counts.iter().map(|(_k, v)| v).min().expect("not empty");
+    let max = counts.iter().map(|(_k, v)| v).max().expect("not empty");
+    println!("Day 14 Pt. 2 answer: {:?}", max - min);
 }
 
 fn expand_template(
@@ -18,16 +24,15 @@ fn expand_template(
     times: usize,
 ) -> HashMap<char, usize> {
     let mut counts = HashMap::new();
+    let mut memoizations = HashMap::new();
 
     for c in template.iter() {
         *counts.entry(*c).or_insert(0) += 1;
     }
 
     template.array_windows::<2>().for_each(|&[a, b]| {
-        println!("Expanding Initial Pair {:?}", (a, b));
-        let expanded_counts = expand_pair((a, b), rules, times);
-        println!(" -> Expanded Counts to: {:?}", expanded_counts);
-        add_counts(&mut counts, expanded_counts)
+        let expanded_counts = expand_pair((a, b), rules, times, &mut memoizations);
+        add_counts(&mut counts, &expanded_counts)
     });
 
     counts
@@ -37,25 +42,36 @@ fn expand_pair(
     (a, b): (char, char),
     rules: &HashMap<(char, char), char>,
     times: usize,
+    memoizations: &mut HashMap<((char, char), usize), HashMap<char, usize>>,
 ) -> HashMap<char, usize> {
     if times == 0 {
         return HashMap::new();
+    }
+
+    if let Some(counts) = memoizations.get(&((a, b), times)) {
+        return counts.clone();
     }
 
     let mut counts = HashMap::new();
 
     if let Some(c) = rules.get(&(a, b)) {
         *counts.entry(*c).or_insert(0) += 1;
-        add_counts(&mut counts, expand_pair((a, *c), rules, times - 1));
-        add_counts(&mut counts, expand_pair((*c, b), rules, times - 1));
+
+        let left_counts = expand_pair((a, *c), rules, times - 1, memoizations);
+        add_counts(&mut counts, &left_counts);
+        memoizations.insert(((a, *c), times - 1), left_counts);
+
+        let right_counts = expand_pair((*c, b), rules, times - 1, memoizations);
+        add_counts(&mut counts, &right_counts);
+        memoizations.insert(((*c, b), times - 1), right_counts);
     }
 
     counts
 }
 
-fn add_counts(to: &mut HashMap<char, usize>, other: HashMap<char, usize>) {
+fn add_counts(to: &mut HashMap<char, usize>, other: &HashMap<char, usize>) {
     for (k, v) in other {
-        *to.entry(k).or_insert(0) += v;
+        *to.entry(*k).or_insert(0) += v;
     }
 }
 
